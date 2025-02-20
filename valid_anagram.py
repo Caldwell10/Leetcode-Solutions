e56ad873ede87a9aa9e7ff49aa6de80d5dd2d6b6
Caldwell10 <caldwell.kibe@strathmore.edu>
2025-02-20 15:55:16 +0300

    Added description

--- valid_anagram.py
+++ valid_anagram.py
@@ -1,4 +1,18 @@
 
+"""
+LeetCode Problem 242: Valid Anagram
+Link: https://leetcode.com/problems/valid-anagram/
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+Example:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Input: s = "rat", t = "car"
+Output: false
+"""
+
 s = "racecar"
 t = "carrace"
 
